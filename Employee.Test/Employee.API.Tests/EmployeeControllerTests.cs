640bd10b0a0351248cfdf67bccb3f17eae9cd6c2
virendra05 <virendraparade01@gmail.com>
2024-12-07 17:48:41 +0530

    Test Project Setup

---
+++ Employee.Test/Employee.API.Tests/EmployeeControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using BusinessManager.Contract;
+using Employee.Api.Controllers;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee.Test.Employee.API.Tests
+{
+    public class EmployeeControllerTests
+    {
+        private readonly Mock<IEmployeeBusinessContract> _employeeBusinessContractMock;
+        private readonly Mock<ILoggingService> _loggerMock;
+        private readonly EmployeeController _employeeController;
+
+        public EmployeeControllerTests()
+        {
+            _loggerMock = new Mock<ILoggingService>();
+            _employeeBusinessContractMock = new Mock<IEmployeeBusinessContract>();
+            _employeeController = new EmployeeController(_employeeBusinessContractMock.Object, _loggerMock.Object);
+        }
+    }
+}
---
+++ Employee.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Employee.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
